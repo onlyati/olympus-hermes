81a44f689cf958c853e54d8b9dff764f8a7c3a6a
Attila Molnar <molnar.attila28@gmail.com>
2022-08-07 21:51:06 +0200

    Minimal cli created

---
+++ cli/src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::process::exit;
+use std::net::TcpStream;
+use std::io::{Write, Read};
+
+fn main() {
+    let mut args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+        println!("Missing input parameters!");
+        exit(1);
+    }
+
+    args.remove(0);
+
+    let mut stream = TcpStream::connect("127.0.0.1:3030").unwrap();
+
+    let full_cmd = args.join(" ");
+    let message = format!("{} {}", full_cmd.len(), full_cmd);
+
+    stream.write(message.as_bytes()).unwrap();
+    
+    let mut response = String::new();
+    stream.read_to_string(&mut response).unwrap();
+    println!("{response}");
+}
