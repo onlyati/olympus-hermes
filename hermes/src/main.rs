435e47f90c9ee9a40a60b49918b14bfcd46aaf01
Attila Molnar <molnar.attila28@gmail.com>
2023-05-18 18:06:11 +0200

    Add initial data reading option

--- hermes/src/main.rs
+++ hermes/src/main.rs
@@ -1,13 +1,18 @@
+use std::collections::HashMap;
+use std::path::Path;
 use std::process::exit;
+use std::sync::mpsc::Sender;
 use std::sync::{Arc, Mutex};
 
 mod interfaces;
 
 use interfaces::classic::Classic;
 use interfaces::grpc::Grpc;
-use interfaces::InterfaceHandler;
 use interfaces::rest::Rest;
 use interfaces::ApplicationInterface;
+use interfaces::InterfaceHandler;
+use onlyati_datastore::enums::DatabaseAction;
+use onlyati_datastore::utilities;
 
 fn main() {
     let rt = tokio::runtime::Builder::new_current_thread()
@@ -36,6 +41,7 @@ async fn main_async() {
 
     // Start datastore thread
     let sender = onlyati_datastore::utilities::start_datastore("root".to_string());
+    parse_input_data("init.data", &config, &sender).unwrap_or_else(|x| panic!("{}", x));
     let sender = Arc::new(Mutex::new(sender));
 
     // Create interface handler
@@ -68,4 +74,83 @@ async fn main_async() {
     // Start interfaces and watch them
     handler.start();
     handler.watch().await; // Block the thread, panic if service failed
+}
+
+/// Parse the input file and upload onto database before anything would happen
+fn parse_input_data(
+    setting_name: &str,
+    config: &HashMap<String, String>,
+    data_sender: &Sender<DatabaseAction>,
+) -> Result<(), String> {
+    if let Some(path) = config.get(setting_name) {
+        let path = Path::new(path);
+        if path.exists() {
+            // Read the file
+            let content = match std::fs::read_to_string(path) {
+                Ok(info) => info,
+                Err(e) => {
+                    return Err(format!(
+                        "Error: Could not read file: {} {}",
+                        path.display(),
+                        e
+                    ))
+                }
+            };
+
+            // Find where ends the key and where the value begin
+            for line in content.lines() {
+                if line.is_empty() {
+                    continue;
+                }
+
+                if &line[0..1] == " " {
+                    continue;
+                }
+
+                let mut end_of_key: usize = 0;
+                let mut start_of_value: usize = 0;
+                let mut index: usize = 0;
+                for char in line.chars() {
+                    if char == ' ' && end_of_key == 0 {
+                        end_of_key = index;
+                        continue;
+                    }
+
+                    if char != ' ' && end_of_key != 0 {
+                        start_of_value = index + 1;
+                        break;
+                    }
+
+                    index += 1;
+                }
+
+                // Allocate strings
+                let key = String::from(&line[0..end_of_key]);
+                let value = String::from(&line[start_of_value..]);
+
+                // Then upload onto database
+                let (tx, rx) = utilities::get_channel_for_set();
+                let action = DatabaseAction::Set(tx, key, value);
+
+                if let Err(e) = data_sender.send(action) {
+                    return Err(format!("Error: {}", e));
+                }
+
+                match rx.recv() {
+                    Ok(response) => match response {
+                        Err(e) => return Err(format!("Error: {}", e)),
+                        _ => (),
+                    },
+                    Err(e) => return Err(format!("Error: {}", e)),
+                }
+            }
+        } else {
+            return Err(format!(
+                "Error: Specified file does not exist: {}",
+                path.display()
+            ));
+        }
+    }
+
+    return Ok(());
 }
