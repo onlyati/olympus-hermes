0a393d0127e424cd83db5ab9b5639ebd9c643d11
Attila Molnar <molnar.attila28@gmail.com>
2023-05-18 18:10:10 +0200

    Add some comment

--- hermes/src/interfaces/mod.rs
+++ hermes/src/interfaces/mod.rs
@@ -1,22 +1,29 @@
-pub struct InterfaceHandler<T> {
-    interfaces: Vec<(String, T)>,
-}
-
 pub mod classic;
 pub mod grpc;
 pub mod rest;
 
+/// # Interface handler
+/// 
+/// Task of interface handler is to start and monitor the specified interfaces like TCP, gRPC or REST.
+/// Interface must implement ApplicationInterface trait.
+pub struct InterfaceHandler<T> {
+    interfaces: Vec<(String, T)>,
+}
+
 impl<T: ApplicationInterface> InterfaceHandler<T> {
+    /// Allocate new interface handler
     pub fn new() -> Self {
         return Self {
             interfaces: Vec::new(),
         };
     }
 
+    /// Function to register interfaces that applied ApplicationInterface trait
     pub fn register_interface(&mut self, interface: T, name: String) {
         self.interfaces.push((name, interface));
     }
 
+    /// Start each registered interface
     pub fn start(&mut self) {
         if self.interfaces.len() == 0 {
             panic!("No interface is registered!");
@@ -27,6 +34,7 @@ impl<T: ApplicationInterface> InterfaceHandler<T> {
         }
     }
 
+    /// Monitor them by an interval, if any interface failes then program has  apanic reaction
     pub async fn watch(&self) {
         loop {
             for interface in &self.interfaces {
@@ -44,11 +52,13 @@ impl<T: ApplicationInterface> InterfaceHandler<T> {
     }
 }
 
+/// Trait that must be implemented that an interface will be able to use InterfaceHandler
 pub trait ApplicationInterface {
     fn run(&mut self);
     fn is_it_run(&self) -> Option<bool>;
 }
 
+/// Boxed implementation of ApplicationInterface trait
 impl ApplicationInterface for Box<dyn ApplicationInterface> {
     fn run(&mut self) {
         return self.as_mut().run();
