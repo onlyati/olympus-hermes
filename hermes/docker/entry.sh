eadd016ae90b3d4fcb83788458f28a8be980a32e
Attila Molnar <molnar.attila28@gmail.com>
2023-05-26 21:23:42 +0200

    Added: docker-compose.yaml file

--- hermes/docker/entry.sh
+++ hermes/docker/entry.sh
@@ -3,5 +3,6 @@ echo "Current directory: $(pwd)"
 
 ls -l /usr/var/hermes/init.dat || mkdir -p "/usr/var/hermes" && echo "" > /usr/var/hermes/init.dat
 ls -l /usr/var/hermes/hook.dat || mkdir -p "/usr/var/hermes" && echo "" > /usr/var/hermes/hook.dat
+ls -l /etc/hermes/main.conf || mkdir -p "/etc/hermes" && cp "/etc/defaults/hermes.conf" "/etc/hermes/main.conf"
 
 ./hermes /etc/hermes/main.conf
